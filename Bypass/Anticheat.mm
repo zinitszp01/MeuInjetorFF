6b43785ed30c5db2e6b6b7c4913653a9cdd05edb
zinitszp01 <viniciuscaue216@gmail.com>
2026-01-29 16:19:09 -0300

    Update Anticheat.mm

--- Bypass/Anticheat.mm
+++ Bypass/Anticheat.mm
@@ -11,27 +11,29 @@ BOOL new_fileExistsAtPath(id self, SEL _cmd, NSString *path) {
     return old_fileExistsAtPath(self, _cmd, path);
 }
 
-// O extern "C" garante que o Tweak.x consiga encontrar esta função
-extern "C" void ExecutarLimpezaBypass() {
-    // 1. LIMPEZA DE LOGS (ANTI-BAN)
-    NSString *docPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    NSFileManager *fm = [NSFileManager defaultManager];
-    
-    NSArray *blacklistFiles = @[
-        @"Logs", @"GarenaSdk", @"Firebase", @"crash_log.txt", 
-        @"client_report_log", @"Pandora", @"il2cpp", @"backtrace"
-    ];
-    
-    for (NSString *file in blacklistFiles) {
-        NSString *path = [docPath stringByAppendingPathComponent:file];
-        if ([fm fileExistsAtPath:path]) {
-            [fm removeItemAtPath:path error:nil];
-            [@"" writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:nil];
+// O extern "C" precisa estar fora e envolvendo a função
+extern "C" {
+    void ExecutarLimpezaBypass() {
+        // 1. LIMPEZA DE LOGS (ANTI-BAN)
+        NSString *docPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+        NSFileManager *fm = [NSFileManager defaultManager];
+        
+        NSArray *blacklistFiles = @[
+            @"Logs", @"GarenaSdk", @"Firebase", @"crash_log.txt", 
+            @"client_report_log", @"Pandora", @"il2cpp", @"backtrace"
+        ];
+        
+        for (NSString *file in blacklistFiles) {
+            NSString *path = [docPath stringByAppendingPathComponent:file];
+            if ([fm fileExistsAtPath:path]) {
+                [fm removeItemAtPath:path error:nil];
+                [@"" writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:nil];
+            }
         }
-    }
 
-    // 2. APLICAÇÃO DE HOOKS (ANTI-BLACKLIST / ANTI-KICK)
-    MSHookMessageEx([NSFileManager class], @selector(fileExistsAtPath:), (IMP)new_fileExistsAtPath, (IMP *)&old_fileExistsAtPath);
-    
-    NSLog(@"[CAU-BYPASS] Proteção Completa Ativada!");
+        // 2. APLICAÇÃO DE HOOKS (ANTI-BLACKLIST / ANTI-KICK)
+        MSHookMessageEx([NSFileManager class], @selector(fileExistsAtPath:), (IMP)new_fileExistsAtPath, (IMP *)&old_fileExistsAtPath);
+        
+        NSLog(@"[CAU-BYPASS] Proteção Completa Ativada!");
+    }
 }
